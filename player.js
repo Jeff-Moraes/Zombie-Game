d8de6596a098ead36568239c5a18d8970d7696e7
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2019-10-30 19:42:05 +0100

    start image

--- player.js
+++ player.js
@@ -2,19 +2,35 @@ let gridSquare = 70;
 
 class Player {
   constructor(x, y) {
-    this.row = y * gridSquare;
+    this.row = y * gridSquare - 10;
     this.col = x * gridSquare;
     this.x = x;
     this.y = y;
     this.position = {};
     this.moves = 3;
+    this.canMove = true;
 
     this.x1 = this.col;
     this.y1 = this.row;
 
+    this.x1Start = -200;
+    this.y1Start = 100;
+
     this.playerStopedFrames = [];
     this.playerRunFrames = [];
     this.frameCounter = 0;
+  }
+
+  newMoves() {
+    setTimeout(() => {
+      this.canMove = true;
+    }, 800);
+  }
+
+  playerDied() {
+    setTimeout(() => {
+      game.gameLevel = 1000;
+    }, 1000);
   }
 
   preload() {
@@ -76,36 +92,50 @@ class Player {
     this.moves -= 1;
   }
   draw() {
-    if (frameCount % 10 === 0) {
-      this.frameCounter = this.frameCounter + 2;
-    }
-
-    if (this.x1 === this.col && this.y1 === this.row) {
-      const playerFrame = this.playerStopedFrames[
-        this.frameCounter % this.playerStopedFrames.length
-      ];
-
-      image(playerFrame, this.x1 - 15, this.y1 - 15, 100, 100);
-    } else {
+    if (game.gameLevel === 0) {
+      if (frameCount % 10 === 0) {
+        this.frameCounter = this.frameCounter + 2;
+      }
       const playerFrame = this.playerRunFrames[
         this.frameCounter % this.playerRunFrames.length
       ];
-      image(playerFrame, this.x1 - 15, this.y1 - 15, 100, 100);
-    }
+      image(playerFrame, this.x1Start, this.y1Start, 300, 300);
+      if (this.x1Start < width + 150) {
+        this.x1Start += 4;
+      } else {
+        this.x1Start = -200;
+      }
+    } else {
+      if (frameCount % 10 === 0) {
+        this.frameCounter = this.frameCounter + 2;
+      }
 
-    if (this.x1 < this.col) {
-      this.x1 += 2;
-    }
-    if (this.y1 < this.row) {
-      this.y1 += 2;
-    }
-    if (this.x1 > this.col) {
-      this.x1 -= 2;
-    }
-    if (this.y1 > this.row) {
-      this.y1 -= 2;
-    }
+      if (this.x1 === this.col && this.y1 === this.row) {
+        const playerFrame = this.playerStopedFrames[
+          this.frameCounter % this.playerStopedFrames.length
+        ];
 
+        image(playerFrame, this.x1 - 15, this.y1 - 15, 100, 100);
+      } else {
+        const playerFrame = this.playerRunFrames[
+          this.frameCounter % this.playerRunFrames.length
+        ];
+        image(playerFrame, this.x1 - 15, this.y1 - 15, 100, 100);
+      }
+
+      if (this.x1 < this.col) {
+        this.x1 += 2;
+      }
+      if (this.y1 < this.row) {
+        this.y1 += 2;
+      }
+      if (this.x1 > this.col) {
+        this.x1 -= 2;
+      }
+      if (this.y1 > this.row) {
+        this.y1 -= 2;
+      }
+    }
     // if (frameCount % 10 === 0) {
     //   this.frameCounter =
     //     (this.frameCounter + 1) % this.playerStopedFrames.length;
